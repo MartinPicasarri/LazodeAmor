c30b0c9ebdf5ed144cc2541d6b66343b3ef4d4b1
Martin Picasarri <martinpicasarri@gmail.com>
2025-05-30 12:55:59 -0300

    Navbar-estilos-transition

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,17 +1,42 @@
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
-import './Navbar.css'; // Importa tus estilos del navbar
+import './Navbar.css'; // Asegúrate de importar tu archivo CSS
 
 const Navbar = () => {
+  const [isTransparent, setIsTransparent] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Define la posición de scroll en la que quieres que empiece la transparencia
+      const scrollThreshold = 100;
+      if (window.scrollY > scrollThreshold) {
+        setIsTransparent(true);
+      } else {
+        setIsTransparent(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Limpia el event listener cuando el componente se desmonte
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const navbarClassName = `navbar ${isTransparent ? 'transparent' : ''}`;
+
   return (
-    <nav className="navbar"> {/* Puedes agregar clases para tus estilos */}
+    <nav id="mainNavbar" className={navbarClassName}>
+      <img src="https://res.cloudinary.com/dkymtbe7c/image/upload/v1748536239/lazodeamorlogo_gi343d.png" alt="Logo" />
       <ul>
         <li>
           <Link
             activeClass="active"
-            to="Home" // El 'name' del Element en tu componente Home
+            to="Home"
             spy={true}
             smooth={true}
-            offset={-70} // Ajusta si tienes un header fijo
+            offset={-70}
             duration={500}
           >
             INICIO
@@ -19,7 +44,7 @@ const Navbar = () => {
         </li>
         <li>
           <Link
-            to="Products" // El 'name' del Element en tu componente Productos
+            to="Products"
             spy={true}
             smooth={true}
             offset={-70}
@@ -30,7 +55,7 @@ const Navbar = () => {
         </li>
         <li>
           <Link
-            to="Contact" // El 'name' del Element en tu componente Contacto
+            to="Contact"
             spy={true}
             smooth={true}
             offset={-70}
